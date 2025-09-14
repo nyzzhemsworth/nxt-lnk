358a4f1421ec01dc7a7b516e9752bd324cf1e624
ziell Ackles <156542084+nyzzhemsworth@users.noreply.github.com>
2025-09-15 03:15:30 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
     title: seoData.openGraph.title,
     excerpt: 'home',
     slug: '/',
-    coverImage: 'https://vjy.me/preview.jpg'
+    coverImage: 'https://files.catbox.moe/ebwten.jpg'
   };
   return (
     <>
